fbfab8649f7179e646050d78cfc2975fb09c59b3
Stefan Countryman <stefan.countryman@gmail.com>
2015-05-09 17:18:17 -0400

    faulty runmetropolis

--- common/md/runmetropolis.jl
+++ common/md/runmetropolis.jl
@@ -1,35 +1,54 @@
 ## QUESTION 2
 
 include("md.jl");
+qstart = loadmdtrial("../../set04/qstart.mdv")
 
 # Pick interaction distance
 δ = 0.1
+threads = 2
+N = 60
+M = 10
+P = int(N/M)
 
-# Create a new simulation object
-q = MolecularDynamicsTrial(
-    1000,               # number of bodies
-    0.75,               # particle density
-    1.069,              # desired temperature; also use 1.304
-    0.032,              # time step dx, in units of τ
-    10000,              # number of iterations
-    3.405e-10,          # length units, from Verlet, in m
-    119.8,              # energy scale for potential, in Kelvin
-    39.948 / 6.0221e26  # argon atomic mass, in kg
-);      
+println("Running ",threads," threads with ",N," steps each and ",P," measurements")
 
-# Place the particles
-mdplace!(q);
+PE = Array(Float64, P, threads)
+KE = Array(Float64, P, threads)
+V  = Array(Float64, P, threads)
 
-# Run for 199 steps
-metropolis!(q, 499, δ)
-
-# Save the trial
-save("set04/q1/q.mdv", q)
-
-# WILL NEED TO ADD A THERMALIZE STEP BACK IN AT SOME POINT
-
-# Run for another 8000 steps, saving all the way
-for i in 1:19
-    metropolis!(q, 500, δ)
-    save("set04/q1/q.mdv", q)
+# Place the particles using result from qtherm
+qs = Array(MolecularDynamicsTrial,threads)
+for i in 1:threads
+    qs[i] = addsteps(qstart, N)     # N new steps
 end
+
+# Since the metropolis algorithm is stochastic and aphysical, we can start from 
+# the thermalized state and just run several simulations in parallel. Set it up
+# for an 8 core google compute engine server. Total of 4.8e4 steps.
+@sync @parallel for i in 1:threads
+    
+    q = qs[i]
+
+    # Store potential and kinetic energy as well as the virial every 10 steps
+    pe = Float64[]
+    ke = Float64[]
+    v  = Float64[]
+
+    # Run for 100 steps at a time, saving values along the way
+    while !isfinished(q)
+        metropolis!(q, M, δ)    # M steps between saves
+        push!(pe, q.pet[q.currentStep])
+        push!(ke, q.ket[q.currentStep])
+        push!(v,  q.vir[q.currentStep])
+    end
+
+    # append results to our collection
+    PE[:,i] = pe
+    KE[:,i] = ke
+    V[:,i]  = v
+end
+
+# save our results
+writedlm("$(pwd())/set04/pe.dat", PE)
+writedlm("$(pwd())/set04/ke.dat", KE)
+writedlm("$(pwd())/set04/v.dat",  V)
